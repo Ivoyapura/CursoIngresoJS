5b08f110cb1710fdbd2e663ef2ce54de43c02625
Ivoyapura <ivoyapura23@hotmail.com>
2021-07-05 17:30:05 -0300

    cambios ejercicios  6 y 7

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,23 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	var numero1;
+	var numero2;
+	numero1=txtIdNumeroUno.value;
+	numero2=txtIdNumeroDos.value;
+	numero1=parseInt(numero1);
+	numero2=parseInt(numero2);
+	resultado=numero1+numero2;
+	alert("el resultado es: "+resultado);
+
+
+
+
+
+
 }
 
+
+
+//txtIdNumeroUno
+// txtIdNumeroDos
--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,54 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	var numero1;
+	var numero2;
+	numero1=txtIdNumeroUno.value;
+	numero2=txtIdNumeroDos.value;
+	numero1=parseInt(numero1);
+	numero2=parseInt(numero2);
+	resultado=numero1+numero2;
+	alert("la suma es "+resultado)
+
 }
 
 function restar()
 {
-	
+	var numero1;
+	var numero2;
+	numero1=txtIdNumeroUno.value;
+	numero2=txtIdNumeroDos.value;
+	numero1=parseInt(numero1);
+	numero2=parseInt(numero2);
+	resultado=numero1-numero2;
+	alert("la resta es "+resultado)
 }
 
 function multiplicar()
 { 
-	
+	var numero1;
+	var numero2;
+	numero1=txtIdNumeroUno.value;
+	numero2=txtIdNumeroDos.value;
+	numero1=parseInt(numero1);
+	numero2=parseInt(numero2);
+	resultado=numero1*numero2;
+	alert("la multiplicacion es "+resultado)
 }
 
 function dividir()
 {
-	
+	var numero1;
+	var numero2;
+	numero1=txtIdNumeroUno.value;
+	numero2=txtIdNumeroDos.value;
+	numero1=parseInt(numero1);
+	numero2=parseInt(numero2);
+	resultado=numero1/numero2;
+	alert("la division es "+resultado)
 }
 
+
+
+//txtIdNumeroUno
+//txtIdNumeroDos
